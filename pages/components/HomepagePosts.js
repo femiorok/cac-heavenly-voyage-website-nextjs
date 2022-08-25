178e4f20ca1c87214df62216d6fa55016bf083ff
femiorok <68767356+femiorok@users.noreply.github.com>
2022-08-24 21:11:14 -0700

    fixing classes

--- pages/components/HomepagePosts.js
+++ pages/components/HomepagePosts.js
@@ -15,7 +15,7 @@ function HomepagePosts({posts}) {
       <div className="block rounded-lg shadow-lg bg-white w-1/4 text-center border-2 border-amber-500 hover:shadow-2xl transition-all ease-in-out">
       <h1 className='text-gray-900 text-xl font-medium py-5 px-3'>{post.title}</h1>
       <p className='text-gray-700 border-t border-gray-300 py-3'>{post.description}</p>
-      <button type="button" class=" inline-block px-6 py-2.5 my-3 bg-amber-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-amber-700 hover:shadow-lg transition duration-150 ease-in-out">Read More</button>
+      <button type="button" className=" inline-block px-6 py-2.5 my-3 bg-amber-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-amber-700 hover:shadow-lg transition duration-150 ease-in-out">Read More</button>
       </div>
       
       </Link>
